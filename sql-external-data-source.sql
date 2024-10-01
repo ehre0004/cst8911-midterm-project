c736d6f4e6cb20547c02786bacb918b50b08f2bf
ehre0004 <ehre0004@algonquinlive.com>
2024-10-01 14:06:15 -0400

    edited to add credential

--- sql-external-data-source.sql
+++ sql-external-data-source.sql
@@ -1,6 +1,6 @@
 CREATE EXTERNAL DATA SOURCE CST8911_Storage
 WITH (
     LOCATION = '', -- example: secure endpoint uses 'abfss://csvFiles@cst8911-midterm-storage.dfs.core.windows.net'
-    CREDENTIAL = -- your credential name here
+    CREDENTIAL = PolyDemoCredential -- your credential name here
     , TYPE = HADOOP
 );
