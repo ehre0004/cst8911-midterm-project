c55404fb1299d9e12d6f6820fea9cffd495ca60b
ehre0004 <ehre0004@algonquinlive.com>
2024-10-01 17:47:31 -0400

    TesT

--- sql-external-data-source.sql
+++ sql-external-data-source.sql
@@ -1,6 +1,6 @@
 CREATE EXTERNAL DATA SOURCE CST8911_Storage
 WITH (
-    LOCATION = '', -- example: secure endpoint uses 'abfss://csvFiles@cst8911-midterm-storage.dfs.core.windows.net'
+    LOCATION = '', -- example: secure endpoint uses 'abfss://csvfiles@cst8911midtermstorage.dfs.core.windows.net'
     CREDENTIAL = PolyDemoCredential -- your credential name here
     , TYPE = HADOOP
 );
