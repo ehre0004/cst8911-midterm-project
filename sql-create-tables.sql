3141f74bbfcf1b44287fb8c21156bf358aedbd0b
ehre0004 <ehre0004@algonquinlive.com>
2024-10-02 17:45:14 -0400

    creating games_ranking csv's associated table

---
+++ sql-create-tables.sql
@@ -0,0 +1,14 @@
+-- Create a temp table to hold the imported data
+CREATE EXTERNAL TABLE dbo.games_ranking (
+    id INT NOT NULL IDENTITY(1,1),
+    game_name VARCHAR(255),
+    genre VARCHAR(255),
+    rank_type VARCHAR(255),
+    rank INT,
+    CONSTRAINT PK_Person PRIMARY KEY (ID,LastName)
+    )
+    WITH (
+            LOCATION = '../'
+            ,DATA_SOURCE = CST8911_Storage
+            ,FILE_FORMAT = csvFormat
+            );
