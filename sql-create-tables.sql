57619ac1890f7d654c5b62d9cc7f48d0809b183c
ehre0004 <ehre0004@algonquinlive.com>
2024-10-08 15:38:25 -0400

    clean csv, changed database table creation file

--- sql-create-tables.sql
+++ sql-create-tables.sql
@@ -26,27 +26,22 @@ CREATE EXTERNAL TABLE dbo.steam_game_reviews (
         ,DATA_SOURCE = CST8911_Storage
         ,FILE_FORMAT = csvFormat
         );
-/* 
+
 CREATE EXTERNAL TABLE dbo.games_description (
     id INT IDENTITY(1,1),
     `name` VARCHAR(100),
-    short_description VARCHAR(255),
-    -- NOTE: delimited list of genres, probably int (0 is not of genre, 1 is of genre)
-    min_system_requirements_os VARCHAR(255),
-    min_system_requirements_graphics_processor VARCHAR(255),
-    -- NOTE: more min system requirements columns from cleaned data
+    short_description VARCHAR(500),
     release_date DATE,
-    developer VARCHAR(50),
-    publisher VARCHAR(50),
-    overall_player_rating_positive int,
-    overall_player_rating_negative int,
+    developer VARCHAR(100),
+    publisher VARCHAR(100),
+    overall_player_rating VARCHAR(50),
     number_of_reviews_from_purchased_people int,
     number_of_english_reviews int,
-    link VARCHAR(150),
+    link VARCHAR(250),
     CONSTRAINT PK_steam_game_reviews_id PRIMARY KEY (id)
     )
     WITH (
         LOCATION = '../'
         ,DATA_SOURCE = CST8911_Storage
         ,FILE_FORMAT = csvFormat
-        ); */
+        ); 
