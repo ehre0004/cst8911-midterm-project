54c9810db5bd739b57d8ae8e47be69ef40d9d472
ehre0004 <ehre0004@algonquinlive.com>
2024-10-18 15:09:54 -0400

    Edited sql-4 create external table file

--- sql-4-create-tables.sql
+++ sql-4-create-tables.sql
@@ -1,10 +1,8 @@
 CREATE EXTERNAL TABLE dbo.games_ranking (
-    id INT IDENTITY(1,1),
     game_name VARCHAR(100),
     genre VARCHAR(100),
     rank_type VARCHAR(100),
-    rank INT,
-    CONSTRAINT PK_games_ranking_id PRIMARY KEY (id)
+    [rank] INT
     )
     WITH (
         LOCATION = '../'
@@ -13,16 +11,14 @@ CREATE EXTERNAL TABLE dbo.games_ranking (
         );
 
 CREATE EXTERNAL TABLE dbo.steam_game_reviews (
-    id INT IDENTITY(1,1),
     hours_played float,
     helpful int,
     funny int,
     recommendation VARCHAR(255),
-    `date` DATE,
+    [date] DATE,
     game_name VARCHAR(255),
-    username VARCHAR(255)
-    num_products_in_account VARCHAR(100),
-    CONSTRAINT PK_steam_game_reviews_id PRIMARY KEY (id)
+    username VARCHAR(255),
+    num_products_in_account VARCHAR(100)
     )
     WITH (
         LOCATION = '../'
@@ -31,8 +27,7 @@ CREATE EXTERNAL TABLE dbo.steam_game_reviews (
         );
 
 CREATE EXTERNAL TABLE dbo.games_description (
-    id INT IDENTITY(1,1),
-    `name` VARCHAR(100),
+    [name] VARCHAR(100),
     short_description VARCHAR(500),
     release_date DATE,
     developer VARCHAR(100),
@@ -40,8 +35,7 @@ CREATE EXTERNAL TABLE dbo.games_description (
     overall_player_rating VARCHAR(50),
     number_of_reviews_from_purchased_people VARCHAR(100),
     number_of_english_reviews int,
-    link VARCHAR(300),
-    CONSTRAINT PK_steam_game_reviews_id PRIMARY KEY (id)
+    link VARCHAR(300)
     )
     WITH (
         LOCATION = '../'
